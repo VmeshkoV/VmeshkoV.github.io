415b98a8870635dbb73f6ec85d19ca1ac2231510
Vladimir Meshkov <vladimir.meshkov@gost-group.com>
2019-04-24 12:23:01 +0300

    Поменял стили

--- ashwinam/js/main.js
+++ ashwinam/js/main.js
@@ -1,3 +1,22 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 });
+
+function getPage(el) {
+  if(el.id === 'about'){
+    // $('.header-menu-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.about').show();
+  } else if(el.id === 'main'){
+    $('.header-menu-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.slider-wrap, .main, .partners').show();
+  } else if(el.id === 'schedule'){
+    $('.header-menu-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.schedule').show();
+  }
+};
