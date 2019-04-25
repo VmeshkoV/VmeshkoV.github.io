d8f95d72e1b347e396526d3f28d2983f55cb9542
VmeshkoV <lapsha1990@gmail.com>
2019-04-25 16:36:43 +0300

    Разработка

--- ashwinam/js/main.js
+++ ashwinam/js/main.js
@@ -1,6 +1,4 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-});
+
 
 function getPage(el) {
   if(el.id === 'about'){
@@ -25,3 +23,64 @@ function getPage(el) {
     $('.ayurveda').show();
   }
 };
+
+
+
+
+function getPageMin(el) {
+  if(el.id === 'about-min'){
+    $('.menu-min-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.about').show();
+  } else if(el.id === 'main-Mind'){
+    $('.menu-min-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.slider-wrap, .main, .partners').show();
+  } else if(el.id === 'schedule-min'){
+    $('.menu-min-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.schedule').show();
+  } else if(el.id === 'ayurveda-min'){
+    $('.menu-min-link').removeClass('active');
+    $(el).addClass('active');
+    $('section').hide();
+    $('.ayurveda').show();
+  }
+};
+
+
+// Открыть и закрыть меню
+$(document).ready(function(){
+
+
+
+
+
+
+
+  $( ".bars-btn" ).click(function() {
+    $( ".menu-min-wrap" ).toggle( "slow", function() {
+      // Animation complete.
+    });
+  });
+});
+
+
+
+
+
+
+
+// Кнопка наверх
+$(document).ready(function(){
+    $('#button-up').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
+
+});
